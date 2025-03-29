4dc9bafed61a217b2a9f9595585b12df86f431ce
Vaishnavi Mahajan <147956035+Vaishh09@users.noreply.github.com>
2025-03-29 14:33:59 -0700

    Initial Commit

---
+++ Swappi/SwappiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwappiApp.swift
+//  Swappi
+//
+//  Created by Vaishnavi Mahajan on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SwappiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwappiTests/SwappiTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwappiTests.swift
+//  SwappiTests
+//
+//  Created by Vaishnavi Mahajan on 3/29/25.
+//
+
+import Testing
+@testable import Swappi
+
+struct SwappiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
